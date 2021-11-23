e6692b47e1fadb749b8b5f01bfb4c72d94801e4d
rez1619 <92747868+rez1619@users.noreply.github.com>
2021-11-22 23:17:40 -0600

    El dios tuvo piedad

--- reportes.cpp
+++ reportes.cpp
@@ -3,6 +3,9 @@
 #include <fstream>
 #include <String.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int op;
@@ -10,172 +13,391 @@ const char archivo1[30] = "productos.txt";
 const char archivo2[30] = "movimientos.txt";
 char *returnStr;
 
-struct NodeProd{
-	int pclave;
-	char pnombre[21];
-	char pfamilia[21];
-	char pmedida[21];
-	int ppu;
-	int peini;
-	int peact;
-	int psmin;
-	int psmax;
-	NodeProd *next;
-	NodeProd *prev;
-};
-
-NodeProd *pprimero, *pactual, *pultimo, *pnuevo, *panterior, *psiguiente, *ptemporal;
-
-struct NodeMov{
-	int mclave;
-	char mfecha[11];
-	int mcantidad;
-	char mmainmov;
-	char msubmov;
-	NodeMov *next;
-	NodeMov *prev;
-};
-
-NodeMov *mprimero, *mactual, *multimo, *mnuevo, *manterior, *msiguiente, *mtemporal;
-
 struct NodeProdSort{
-	int pclave;
-	char pnombre[21];
-	char pfamilia[21];
-	char pmedida[21];
-	int ppu;
-	int peini;
-	int peact;
-	int psmin;
-	int psmax;
+	int spclave;
+	char spnombre[21];
+	char spfamilia[21];
+	char spmedida[21];
+	int sppu;
+	int speini;
+	int speact;
+	int spsmin;
+	int spsmax;
 	NodeProdSort *next;
 	NodeProdSort *prev;
 };
 
-NodeProdSort *ppprimero, *ppactual, *ppultimo, *ppnuevo, *ppanterior, *ppsiguiente, *pptemporal;
+NodeProdSort *spprimero, *spactual, *spultimo, *spnuevo, *spanterior, *spsiguiente, *sptemporal;
 
 struct NodeMovSort{
-	int mclave;
-	char mfecha[11];
-	int mcantidad;
-	char mmainmov;
-	char msubmov;
+	int smclave;
+	char smfecha[11];
+	int smcantidad;
+	char smmainmov;
+	char smsubmov;
 	NodeMovSort *next;
 	NodeMovSort *prev;
 };
 
-NodeMovSort *mmprimero, *mmactual, *mmultimo, *mmnuevo, *mmanterior, *mmsiguiente, *mmtemporal;
+NodeMovSort *smprimero, *smactual, *smultimo, *smnuevo, *smanterior, *smsiguiente, *smtemporal;
 
-char *pedir_cadena(int limite1, int limite2, char const *stonks) {
-	int limiteReal1= limite1+1;
-	int limiteReal2= limite2+1;
-	char *x;
-	x = new char[limite2+2];
-	do {
-		printf("%s entre %d y %d : ", stonks, limite1, limite2); fgets(x, limiteReal2+2, stdin); fflush(stdin);
-		if ((strlen(x)<limiteReal1) || (strlen(x)>limiteReal2)) {
-			printf("Error, cadena fuera del rango. Intente de nuevo.\n");
-			getche();
-		}
-	} while ((strlen(x)<limiteReal1) || (strlen(x)>limiteReal2));
-	if (x[strlen(x)-1] == '\n'){
-		x[strlen(x)-1]='\0';
-	}
-	returnStr = (char *)malloc((limite2+2) * sizeof(char));
-	strcpy(returnStr, x);
-	delete x;
-	return returnStr;
-}
-
-char *pedir_cadena_sinespacios(int limite1, int limite2, char const *stonks) {
-	int limiteReal1= limite1+1;
-	int limiteReal2= limite2+1;
-	char *x;
-	x = new char[limite2+2];
-	do {
-		printf("%s entre %d y %d : ", stonks, limite1, limite2); fgets(x, limiteReal2+2, stdin); fflush(stdin);
-		if ((strlen(x)<limiteReal1) || (strlen(x)>limiteReal2)) {
-			printf("Error, cadena fuera del rango. Intente de nuevo.\n");
-			getche();
-		}
-	} while ((strlen(x)<limiteReal1) || (strlen(x)>limiteReal2));
-	for (int i = 0; i<strlen(x); i++){
-		if (x[i]==' '){
-			x[i]='_';
-		}
-	}
-	if (x[strlen(x)-1] == '\n'){
-		x[strlen(x)-1]='\0';
-	}
-	returnStr = (char *)malloc((limite2+2) * sizeof(char));
-	strcpy(returnStr, x);
-	delete x;
-	return returnStr;
-}
-
-int pedir_entero(int limite1, int limite2, char const *stonks) {
-	int x;
-	do {
-		printf("%s entre %d y %d : ", stonks, limite1, limite2); scanf("%d", &x); fflush(stdin);
-		if ((x<limite1) || (x>limite2)) {
-			printf("Error, numero fuera del rango. Intente de nuevo.\n");
-			getche();
-		}
-	} while ((x<limite1) || (x>limite2));
-	return x;
-}
-
-void fromProdToNode(){
-	ifstream arch;
-	arch.open(archivo1, ios::in);
-	while(!arch.eof()){
-		int clave;
-		char nombre[21];
-		char familia[21];
-		char medida[21];
-		int pu;
-		int eini;
-		int eact;
-		int smin;
-		int smax;
-		arch >>clave>>nombre>>familia>>medida>>pu>>eini>>eact>>smin>>smax;
-		if(!arch.eof()){
-			pnuevo = new NodeProd;
-			pnuevo -> pclave = clave;
-			strcpy(pnuevo->pnombre, nombre);
-			strcpy(pnuevo -> pfamilia, familia);
-			strcpy(pnuevo -> pmedida, medida);
-			pnuevo -> ppu = pu;
-			pnuevo -> peini = eini;
-			pnuevo -> peact = eact;
-			pnuevo -> psmin = smin;
-			pnuevo -> psmax = smax;
-			pnuevo -> next = NULL;
-			if(pprimero == NULL){
-				pprimero=pnuevo;
-				pultimo=pnuevo;
-			}else{
-				pultimo -> next = pnuevo;
-				pnuevo -> prev = pultimo;
-				pultimo = pnuevo;
+void inserta_productos_sort(int clv, char nmb[21], char fam[21], char med[21], int uni, int ini, int act, int smn, int smx, int pos){
+    spnuevo=new NodeProdSort;
+    spnuevo->spclave=clv;
+    strcpy(spnuevo->spnombre,nmb);
+    strcpy(spnuevo->spfamilia,fam);
+    strcpy(spnuevo->spmedida,med);
+    spnuevo->sppu=uni;
+    spnuevo->speini=ini;
+    spnuevo->speact=act;
+    spnuevo->spsmin=smn;
+    spnuevo->spsmax=smx;
+    spnuevo->next=NULL;
+    spnuevo->prev=NULL;
+    if(spprimero==NULL){
+        spprimero=spnuevo;
+        spultimo=spnuevo;
+    }
+    else{
+		spanterior = NULL;
+		sptemporal = spprimero;
+		int count = 1;
+		while(sptemporal!=NULL){
+			if(count == pos){
+				break;
 			}
+			spanterior = sptemporal;
+			sptemporal = sptemporal->next;
+			count++;
 		}
-	}
+		if(spanterior == NULL){
+			sptemporal->prev=spnuevo;
+			spnuevo->next=sptemporal;
+			spnuevo->prev=spanterior;
+			spprimero=spnuevo;
+		}
+		else if(sptemporal == NULL){
+			spnuevo->next=sptemporal;
+			spanterior->next=spnuevo;
+			spnuevo->prev=spanterior;
+			spultimo=spnuevo;
+		}else{
+			sptemporal->prev=spnuevo;
+			spnuevo->next=sptemporal;
+			spanterior->next=spnuevo;
+			spnuevo->prev=spanterior;
+		}
+    }
 }
 
-void testmuestra(){
-	if (pprimero==NULL){
-		printf("No hay nodos.\n");
+void inserta_movimientos_sort(int clave, char fecha[11], int cantidad, char mainmov, char submov, int pos){
+    smnuevo=new NodeMovSort;
+    smnuevo->smclave=clave;
+    strcpy(smnuevo->smfecha,fecha);
+    smnuevo->smcantidad=cantidad;
+    smnuevo->smmainmov=mainmov;
+    smnuevo->smsubmov=submov;
+    smnuevo->next = NULL;
+    if(smprimero==NULL){
+        smprimero=smnuevo;
+        smultimo=smnuevo;
+    }
+    else{
+		smanterior = NULL;
+		smtemporal = smprimero;
+		int count = 1;
+		while(smtemporal!=NULL){
+			if(count == pos){
+				break;
+			}
+			smanterior = smtemporal;
+			smtemporal = smtemporal->next;
+			count++;
+		}
+		if(smanterior == NULL){
+			smtemporal->prev=smnuevo;
+			smnuevo->next=smtemporal;
+			smnuevo->prev=smanterior;
+			smprimero=smnuevo;
+		}
+		else if(smtemporal == NULL){
+			smnuevo->next=smtemporal;
+			smanterior->next=smnuevo;
+			smnuevo->prev=smanterior;
+			smultimo=smnuevo;
+		}else{
+			smtemporal->prev=smnuevo;
+			smnuevo->next=smtemporal;
+			smanterior->next=smnuevo;
+			smnuevo->prev=smanterior;
+		}
+    }
+}
+
+void muestraProd(){
+	if (spprimero==NULL){
+		printf("Error! No hay nodos para mostrar. Esto puede significar que no hay registros, o no hay registros para el filtro especificado.\n");
 		getche();
 		return;
 	}else{
-		pactual = pprimero;
+		spactual = spprimero;
+		printf("===============================================================================================================\n");
+		printf("CLAVE                NOMBRE               FAMILIA               UNI/MED    PR/U   EXINI   EXACT   STMIN   STMAX\n");
+		printf("===============================================================================================================\n");
 		do{
-			printf("%d\n", pactual->pclave);
-			pactual=pactual->next;
-		}while(pactual!=NULL);
+			printf("%5d  %20s  %20s  %20s  %6d  %6d  %6d  %6d  %6d\n", spactual->spclave, spactual->spnombre, spactual->spfamilia, spactual->spmedida, spactual->sppu, spactual->speini, spactual->speact, spactual->spsmin, spactual->spsmax);
+			spactual=spactual->next;
+		}while(spactual!=NULL);
+		printf("===============================================================================================================\n");
+		printf("Presione Enter para continuar...");
 		getche();
 	}
+}
+
+void muestraMovi(){
+	if (smprimero==NULL){
+		printf("Error! No hay nodos para mostrar. Esto puede significar que no hay registros, o no hay registros para el filtro especificado.\n");
+		getche();
+		return;
+	}else{
+		smactual = smprimero;
+		printf("====================================================\n");
+		printf("CLAVE  FECHA       CANTIDAD   TIPO_MOV  SUB_TIPO_MOV\n");
+		printf("====================================================\n");
+		do{
+			printf("%5d  %10s    %6d      %c            %c\n", smactual->smclave, smactual->smfecha, smactual->smcantidad, smactual->smmainmov, smactual->smsubmov);
+			smactual=smactual->next;
+		}while(smactual!=NULL);
+		printf("====================================================\n");
+		printf("Presione Enter para continuar...");
+		getche();
+	}
+}
+
+void sortClave(){
+	pactual = pprimero;
+	spprimero = NULL;
+	spactual = NULL;
+	spultimo = NULL;
+	spnuevo = NULL;
+	spanterior = NULL;
+	spsiguiente = NULL;
+	sptemporal = NULL;
+	do{
+		if (spprimero==NULL){
+			inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, 1);
+		}else{
+			int pos = 1;
+			int check = 0;
+			spactual = spprimero;
+			do{
+				if (pactual->pclave < spactual->spclave){
+					inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, pos);
+					check = 1;
+					break;
+				}
+				pos++;
+				spactual = spactual->next;
+			}while(spactual!=NULL);
+			if (check == 0){
+				inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, pos);
+			}
+		}
+		pactual=pactual->next;
+	}while(pactual!=NULL);
+}
+
+void sortNombre(){
+	pactual = pprimero;
+	spprimero = NULL;
+	spactual = NULL;
+	spultimo = NULL;
+	spnuevo = NULL;
+	spanterior = NULL;
+	spsiguiente = NULL;
+	sptemporal = NULL;
+	do{
+		if (spprimero==NULL){
+			inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, 1);
+		}else{
+			int pos = 1;
+			int check = 0;
+			int n = 0;
+			string nombrep;
+			string nombres;
+			spactual = spprimero;
+			do{
+				int n = 0;
+				nombrep = pactual -> pnombre;
+				nombres = spactual -> spnombre;
+				int i = 0;
+				while( nombrep[i] ) {
+					nombrep[i] = tolower(nombrep[i]);
+					i++;
+				}
+				i = 0;
+				while( nombres[i] ) {
+					nombres[i] = tolower(nombres[i]);
+					i++;
+				}
+				while(true){
+					if(nombrep.substr(0, n) == nombres.substr(0, n)){
+						n++;
+						continue;
+					}else{
+						break;
+					}
+				}
+				if (nombrep.substr(0, n) < nombres.substr(0, n)){
+					inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, pos);
+					check = 1;
+					break;
+				}
+				pos++;
+				spactual = spactual->next;
+			}while(spactual!=NULL);
+			if (check == 0){
+				inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, pos);
+			}
+		}
+		pactual=pactual->next;
+	}while(pactual!=NULL);
+}
+
+void sortExistenciaOr(){
+	pactual = pprimero;
+	spprimero = NULL;
+	spactual = NULL;
+	spultimo = NULL;
+	spnuevo = NULL;
+	spanterior = NULL;
+	spsiguiente = NULL;
+	sptemporal = NULL;
+	do{
+		if (pactual->peact < pactual->psmin){
+			if (spprimero==NULL){
+				inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, 1);
+			}else{
+				int pos = 1;
+				int check = 0;
+				spactual = spprimero;
+				do{
+					if (pactual->peact < spactual->speact){
+						inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, pos);
+						check = 1;
+						break;
+					}
+					pos++;
+					spactual = spactual->next;
+				}while(spactual!=NULL);
+				if (check == 0){
+					inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, pos);
+				}
+			}
+			pactual=pactual->next;
+		}else{
+			pactual=pactual->next;
+		}
+	}while(pactual!=NULL);
+}
+
+void sortExistenciaOf(){
+	pactual = pprimero;
+	spprimero = NULL;
+	spactual = NULL;
+	spultimo = NULL;
+	spnuevo = NULL;
+	spanterior = NULL;
+	spsiguiente = NULL;
+	sptemporal = NULL;
+	do{
+		if (pactual->peact > pactual->psmax){
+			if (spprimero==NULL){
+				inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, 1);
+			}else{
+				int pos = 1;
+				int check = 0;
+				spactual = spprimero;
+				do{
+					if (pactual->peact < spactual->speact){
+						inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, pos);
+						check = 1;
+						break;
+					}
+					pos++;
+					spactual = spactual->next;
+				}while(spactual!=NULL);
+				if (check == 0){
+					inserta_productos_sort(pactual->pclave, pactual->pnombre, pactual->pfamilia, pactual->pmedida, pactual->ppu, pactual->peini, pactual->peact, pactual->psmin, pactual->psmax, pos);
+				}
+			}
+			pactual=pactual->next;
+		}else{
+			pactual=pactual->next;
+		}
+	}while(pactual!=NULL);
+}
+
+void sortFecha(){
+	mactual = mprimero;
+	smprimero = NULL;
+	smactual = NULL;
+	smultimo = NULL;
+	smnuevo = NULL;
+	smanterior = NULL;
+	smsiguiente = NULL;
+	smtemporal = NULL;
+	/*int smclave;
+	char smfecha[11];
+	int smcantidad;
+	char smmainmov;
+	char smsubmov;*/
+	do{
+		if (smprimero==NULL){
+			inserta_movimientos_sort(mactual->mclave, mactual->mfecha, mactual->mcantidad, mactual->mmainmov, mactual->msubmov, 1);
+		}else{
+			int pos = 1;
+			int check = 0;
+			smactual = smprimero;
+			string fechan;
+			string fechas;
+			string dian;
+			string dias;
+			string mesn;
+			string mess;
+			string yean;
+			string yeas;
+			do{
+				fechan = mactual -> mfecha;
+				fechas = smactual -> smfecha;
+				dian = fechan.substr(0, 2);
+				dias = fechas.substr(0, 2);
+				mesn = fechan.substr(3, 2);
+				mess = fechas.substr(3, 2);
+				yean = fechan.substr(6, 4);
+				yeas = fechas.substr(6, 4);
+				if (std::stoi(yean) < std::stoi(yeas)){
+					inserta_movimientos_sort(mactual->mclave, mactual->mfecha, mactual->mcantidad, mactual->mmainmov, mactual->msubmov, pos);
+					check = 1;
+					break;
+				}else if (std::stoi(yean) == std::stoi(yeas) && std::stoi(mesn) < std::stoi(mess)){
+					inserta_movimientos_sort(mactual->mclave, mactual->mfecha, mactual->mcantidad, mactual->mmainmov, mactual->msubmov, pos);
+					check = 1;
+					break;
+				}else if (std::stoi(mesn) == std::stoi(mess) && std::stoi(dian) <= std::stoi(dias)){
+					inserta_movimientos_sort(mactual->mclave, mactual->mfecha, mactual->mcantidad, mactual->mmainmov, mactual->msubmov, pos);
+					check = 1;
+					break;
+				}
+				pos++;
+				smactual = smactual->next;
+			}while(smactual!=NULL);
+			if (check == 0){
+				inserta_movimientos_sort(mactual->mclave, mactual->mfecha, mactual->mcantidad, mactual->mmainmov, mactual->msubmov, pos);
+			}
+		}
+		mactual=mactual->next;
+	}while(mactual!=NULL);
 }
 
 void menuReportes(){
@@ -194,18 +416,28 @@ void menuReportes(){
 		strcpy(x, pedir_cadena(1, 1, "Indica la opcion"));
 		free(returnStr);
 		if (strcmp(x, "a") == 0){
-			fromProdToNode();
-			testmuestra();
+			sortClave();
+			muestraProd();
 		}else if (strcmp(x, "b") == 0){
-
+			sortNombre();
+			muestraProd();
 		}else if (strcmp(x, "c") == 0){
-			
+			//consulta_familia()
 		}else if (strcmp(x, "d") == 0){
-			
+			sortExistenciaOr();
+			muestraProd();
 		}else if (strcmp(x, "e") == 0){
-			
+			sortExistenciaOf();
+			muestraProd();
 		}else if (strcmp(x, "f") == 0){
-			
+			pclave=pedir_entero(1,99999,"Indica la clave del producto a buscar");
+    		if(!busca_clave(pclave)){
+				printf("\nError, la clave no existe en productos.\n");
+				getche();
+			}else{
+			sortFecha();
+			muestraMovi();
+			}
 		}else if (strcmp(x, "x") == 0){
 			printf("Gracias por usar el programa...\n");
 			getche();
@@ -217,10 +449,12 @@ void menuReportes(){
 }
 
 main(){
-	ofstream arch;
-	arch.open(archivo1, ios::app);
-	arch.close();
-	arch.open(archivo2, ios::app);
-	arch.close();
+	std::ofstream file;
+    file.open("productos.txt",std::ios::app);
+    file.close();
+    file.open("movimientos.txt",std::ios::app);
+    file.close();
+    cargar_productos("productos.txt");
+    cargar_movimientos("movimientos.txt");
 	menuReportes();
 }
